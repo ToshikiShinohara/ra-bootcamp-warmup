5c09fb5093336a183e792758e69eb4fc63cc6ee9
Shinohara Toshiki <shinoariyouaya@gmail.com>
2024-08-18 13:43:52 +0900

    Add gradrate clean data and code

--- 01_cleaning/cleaning_code_file.R
+++ 01_cleaning/cleaning_code_file.R
@@ -34,6 +34,29 @@ semester_clean_data <- semester_grouped_data
 write_csv(semester_clean_data, file = "01_cleaning/cleaning_data/semester_clean_data.csv")
 
 
-#----(b)Graduate Dataの整形----
+#----(b)Gradrate Dataの整形----
 
 #生データの読み込み
+years <- c(1991:1993, 1995:2010) #読み込む年の指定
+file_paths <- paste0("01_cleaning/gradrate_raw_data/", years, ".xlsx") #ファイルパスの作成
+gradrate_raw_data_list <- map(file_paths, read_excel) #データの読み込み
+gradrate_combined_data <- bind_rows(gradrate_raw_data_list) #データの結合
+
+#女子学生の4年卒業率のスケール変化
+gradrate_combined_data$womengradrate4yr <- gradrate_combined_data$women_gradrate_4yr*0.01
+
+#男子学生の4年卒業率算出
+gradrate_combined_data$m_4yrgrads <- as.numeric(gradrate_combined_data$m_4yrgrads)
+gradrate_combined_data$mengradrate4yr <- gradrate_combined_data$m_4yrgrads / gradrate_combined_data$m_cohortsize
+
+#男女合計の4年卒業率算出
+gradrate_combined_data$totcohortsize <- as.numeric(gradrate_combined_data$totcohortsize)
+gradrate_combined_data$gradrate4yr <- gradrate_combined_data$tot4yrgrads / gradrate_combined_data$totcohortsize 
+
+#有効数字を3桁に調整
+gradrate_combined_data$mengradrate4yr <- round(gradrate_combined_data$mengradrate4yr, 3)
+gradrate_combined_data$gradrate4yr <- round(gradrate_combined_data$gradrate4yr, 3)
+
+#クリーニング後データの出力
+gradrate_clean_data <- data.frame(gradrate_combined_data)
+write_csv(gradrate_clean_data, file = "01_cleaning/cleaning_data/gradrate_clean_data.csv")
